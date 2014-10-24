38e43983baa7baef5cc024b58836d5553057bb76
yang-wei <yangweilim21@gmail.com>
2014-10-25 01:45:07 +0900

    update Coin and Lava class

--- app/scripts/Lava.js
+++ app/scripts/Lava.js
@@ -1,7 +1,25 @@
 ;(function() {
 
     'use strict';
-    
 
+    // Lava type
+    // moving horizontally -> =
+    // moving vertically   -> |
+    // dripping lava       -> v
+    function Lava(pos, type) {
+        this.pos = pos;
+        this.size = new Vector(1, 1);
+
+        if (type === "=") {
+            this.speed = new Vector(2, 0);
+        } else if (type === "|") {
+            this.speed = new Vector(0, 2); 
+        } else if (type === "v") {
+            this.speed = new Vector(0, 2);
+            this.repeatPos = pos;
+        }
+    }    
+    
+    Lava.prototype.type = "Lava";
 
 })();
