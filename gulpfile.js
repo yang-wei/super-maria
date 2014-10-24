109eebb0d7bfd18d8b44032f4c6c7226169fd573
yang-wei <yangweilim21@gmail.com>
2014-10-24 23:11:56 +0900

    set up local server

---
+++ gulpfile.js
@@ -0,0 +1,26 @@
+var gulp = require('gulp'),
+    connect = require('gulp-connect');
+
+gulp.task('connect', function() {
+    connect.server({
+        root: 'app',
+        livereload: true
+    })  
+});
+
+gulp.task('html', function() {
+    gulp.src('./app/*.html')
+        .pipe(connect.reload());
+});
+
+gulp.task('script', function() {
+    gulp.src('./app/*.js')
+        .pipe(connect.reload());
+});
+
+gulp.task('watch', function() {
+    gulp.watch(['./app/*.html'], ['html']);
+    gulp.watch(['./app/*.js'], ['script']);
+});
+
+gulp.task('default', ['connect', 'watch']);
